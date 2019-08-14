d7f1cc7fc9b5e8ca55e95d6d14ec2fc4981ec230
DanielChisica <danielchisica777@gmail.com>
2019-08-14 16:32:10 -0500

    Ejercicio 2 funcionando

---
+++ pepesRoutine.js
@@ -0,0 +1,110 @@
+const fs = require('fs'),
+    file='./games.txt',
+    newFile='./games_Promises.txt',
+    promise= new Promise((resolve,reject)=>{
+        let resolveMsg='06:00am he wake up'
+        let rejectMsg='Alarm did not ring'
+        setTimeout(()=>{
+            if(true){
+                resolve(resolveMsg)
+            } else {
+                reject(new Error(rejectMsg))
+            }
+        },2000)
+    }).then((dataPromise)=>{
+        console.log(dataPromise)
+        return new Promise((resolve,reject)=>{
+            let resolveMsg='07:00am he is ready and goes out to take TransMilenio'
+            let rejectMsg='strike in the portal and cannot take transport'
+            setTimeout(()=>{
+                if(true){
+                    resolve(resolveMsg)
+                } else {
+                    reject(new Error(rejectMsg))
+                }
+            },2000)
+        })
+    }).then((dataPromise)=>{
+        console.log(dataPromise)
+        return new Promise((resolve,reject)=>{
+            let resolveMsg='08:15 am enter to the classroom'
+            let rejectMsg='He is late and can not entry'
+            setTimeout(()=>{
+                if(false){
+                    resolve(resolveMsg)
+                } else {
+                    reject(new Error(rejectMsg))
+                }
+            },2000)
+        })
+    }).then((dataPromise)=>{
+        console.log(dataPromise)
+        return new Promise((resolve,reject)=>{
+            let resolveMsg='12:00 goes out of class and eat lunch'
+            let rejectMsg='He cannot lunch because the soup has a hair'
+            setTimeout(()=>{
+                if(true){
+                    resolve(resolveMsg)
+                } else {
+                    reject(new Error(rejectMsg))
+                }
+            },2000)
+        })
+    }).then((dataPromise)=>{
+        console.log(dataPromise)
+        return new Promise((resolve,reject)=>{
+            let resolveMsg='01:00pm goes to his job at Bellatrix'
+            let rejectMsg='forgot his badge and cannot entry'
+            setTimeout(()=>{
+                if(true){
+                    resolve(resolveMsg)
+                } else {
+                    reject(new Error(rejectMsg))
+                }
+            },2000)
+        })
+    }).then((dataPromise)=>{
+        console.log(dataPromise)
+        return new Promise((resolve,reject)=>{
+            let resolveMsg='08:00pm goes out of job to hangout with his workmates'
+            let rejectMsg='He is stolen'
+            setTimeout(()=>{
+                if(true){
+                    resolve(resolveMsg)
+                } else {
+                    reject(new Error(rejectMsg))
+                }
+            },2000)
+        })
+    }).then((dataPromise)=>{
+        console.log(dataPromise)
+        return new Promise((resolve,reject)=>{
+            let resolveMsg='11:00pm comeback to home'
+            let rejectMsg='He has lost his home keys and he has to sleep at park'
+            setTimeout(()=>{
+                if(true){
+                    resolve(resolveMsg)
+                } else {
+                    reject(new Error(rejectMsg))
+                }
+            },2000)
+        })
+    }).then((dataPromise)=>{
+        console.log(dataPromise)
+        return new Promise((resolve,reject)=>{
+            let resolveMsg='01:00am he goes to bed to watch Netflix'
+            let rejectMsg='Forgot the monthly payment of Netflix... the bed is broken and he has not place to sleep'
+            setTimeout(()=>{
+                if(true){
+                    resolve(resolveMsg)
+                } else {
+                    reject(new Error(rejectMsg))
+                }
+            },2000)
+        })
+    }).then((dataPromise)=>{
+        console.log(dataPromise)
+    }).catch((err)=>{
+        console.log(err.message)
+    }
+    )
