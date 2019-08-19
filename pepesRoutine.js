7e6c27b9480efcb3c030e699a6d20d459ec68287
DanielChisica <danielchisica777@gmail.com>
2019-08-19 02:21:19 -0500

    Implementing routing and views

--- pepesRoutine.js
+++ pepesRoutine.js
@@ -1,11 +1,29 @@
-const fs = require('fs'),
-    file='./games.txt',
-    newFile='./games_Promises.txt',
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const products = [];
+const jsonParser = bodyParser.json();
+
+var promise;
+app.set('port',process.env.PORT || 3000)
+app.use(express.static(path.join(__dirname,'pages')));
+
+app.listen(app.get('port'), () => {
+    console.log(`server on port ${app.get('port')}`);
+});
+
+app.post('/index',jsonParser,(req,res)=>{
+    console.log(req.body)
+    console.log(typeof req.body)
+    console.log(`${req.method} body : ${req.body} request for ${req.url}`);
     promise= new Promise((resolve,reject)=>{
         let resolveMsg='06:00am he wake up'
         let rejectMsg='Alarm did not ring'
         setTimeout(()=>{
-            if(true){
+            if(req.body.bool1==false){
                 resolve(resolveMsg)
             } else {
                 reject(new Error(rejectMsg))
@@ -17,7 +35,7 @@ const fs = require('fs'),
             let resolveMsg='07:00am he is ready and goes out to take TransMilenio'
             let rejectMsg='strike in the portal and cannot take transport'
             setTimeout(()=>{
-                if(true){
+                if(req.body.bool2==false){
                     resolve(resolveMsg)
                 } else {
                     reject(new Error(rejectMsg))
@@ -30,7 +48,7 @@ const fs = require('fs'),
             let resolveMsg='08:15 am enter to the classroom'
             let rejectMsg='He is late and can not entry'
             setTimeout(()=>{
-                if(false){
+                if(req.body.bool3==false){
                     resolve(resolveMsg)
                 } else {
                     reject(new Error(rejectMsg))
@@ -43,7 +61,7 @@ const fs = require('fs'),
             let resolveMsg='12:00 goes out of class and eat lunch'
             let rejectMsg='He cannot lunch because the soup has a hair'
             setTimeout(()=>{
-                if(true){
+                if(req.body.bool4==false){
                     resolve(resolveMsg)
                 } else {
                     reject(new Error(rejectMsg))
@@ -56,7 +74,7 @@ const fs = require('fs'),
             let resolveMsg='01:00pm goes to his job at Bellatrix'
             let rejectMsg='forgot his badge and cannot entry'
             setTimeout(()=>{
-                if(true){
+                if(req.body.bool5==false){
                     resolve(resolveMsg)
                 } else {
                     reject(new Error(rejectMsg))
@@ -69,7 +87,7 @@ const fs = require('fs'),
             let resolveMsg='08:00pm goes out of job to hangout with his workmates'
             let rejectMsg='He is stolen'
             setTimeout(()=>{
-                if(true){
+                if(req.body.bool6==false){
                     resolve(resolveMsg)
                 } else {
                     reject(new Error(rejectMsg))
@@ -82,7 +100,7 @@ const fs = require('fs'),
             let resolveMsg='11:00pm comeback to home'
             let rejectMsg='He has lost his home keys and he has to sleep at park'
             setTimeout(()=>{
-                if(true){
+                if(req.body.bool7==false){
                     resolve(resolveMsg)
                 } else {
                     reject(new Error(rejectMsg))
@@ -95,7 +113,7 @@ const fs = require('fs'),
             let resolveMsg='01:00am he goes to bed to watch Netflix'
             let rejectMsg='Forgot the monthly payment of Netflix... the bed is broken and he has not place to sleep'
             setTimeout(()=>{
-                if(true){
+                if(req.body.bool8==false){
                     resolve(resolveMsg)
                 } else {
                     reject(new Error(rejectMsg))
@@ -104,7 +122,11 @@ const fs = require('fs'),
         })
     }).then((dataPromise)=>{
         console.log(dataPromise)
+        res.json('Successfully directed');
     }).catch((err)=>{
         console.log(err.message)
-    }
+        res.json('Successfully directed');
+        }
     )
+})
+
